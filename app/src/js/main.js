06cdad677972104be859b709f780df553f5e5db4
Lisa <lisabazdyreva@gmail.com>
2023-04-21 16:33:22 +0300

    added support of dark mode and added fonts

--- app/src/js/main.js
+++ app/src/js/main.js
@@ -3,7 +3,12 @@ const LanguageValue = {
   English: "en",
 };
 
+const NightModeValue = {
+  Dark: "dark-mode",
+};
+
 const languageButton = document.querySelector(".language-button");
+const nightModeButton = document.querySelector(".night-mode-button");
 
 const changeLanguage = () => {
   const lang = document.documentElement.lang;
@@ -20,10 +25,28 @@ const changeLanguage = () => {
   }
 };
 
+const changeNightMode = () => {
+  const currentMode = document.body.classList.contains(NightModeValue.Dark);
+
+  if (currentMode) {
+    document.body.classList.remove(NightModeValue.Dark);
+  } else {
+    document.body.classList.add(NightModeValue.Dark);
+  }
+};
+
 const onChangeLanguageButtonClickHandler = () => {
   changeLanguage();
 };
 
+const onNightModeButtonClickHandler = () => {
+  changeNightMode();
+};
+
 if (languageButton) {
   languageButton.addEventListener("click", onChangeLanguageButtonClickHandler);
+}
+
+if (nightModeButton) {
+  nightModeButton.addEventListener("click", onNightModeButtonClickHandler);
 }
